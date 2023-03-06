98f7685840a32f7ac493d86177dcca4158822a77
adenmengistie <aden.mengistie@gmail.com>
2023-03-05 16:24:24 -0800

    Modification to parent process and namesModify

--- countnames_parallel.c
+++ countnames_parallel.c
@@ -12,12 +12,11 @@ typedef struct unique_name{
     int count;
 }unique_name;
 
-void namesModify(struct unique_name names[NUM_NAME], char tempName[NAME_LENGTH]);
+void namesModify(struct unique_name names[NUM_NAME], struct unique_name tempName);
 void display(struct unique_name names[NUM_NAME]);
 
 int main(int argc, char *argv[]) {
     struct unique_name names[NUM_NAME] = {0};
-    char temp[NAME_LENGTH];
     pid_t child;
     int fd[2];
     pipe(fd);
@@ -28,7 +27,6 @@ int main(int argc, char *argv[]) {
     }
     for (int i = 1; i < argc; i++) {
         child = fork();
-
         if (child < 0) {
             printf("Error with fork for processing file %s.\n", argv[i]);
             exit(1);
@@ -38,46 +36,52 @@ int main(int argc, char *argv[]) {
                 printf("Unable to open %s.\n", argv[i]);
                 exit(1);
             }
+            char temp[NAME_LENGTH];
+            struct unique_name tempName;
             int line = 0;
             while (fgets(temp, NAME_LENGTH, fp)) {
                 line++;
                 if (strcmp("\n", temp) == 0 || strcmp(" \n", temp) == 0) {
-                    fprintf(stderr, "Warning - file %s line %d is empty.", argv[i], line);
+                    fprintf(stderr, "Warning - file %s line %d is empty.\n", argv[i], line);
                 } else {
                     char *newlineCheck = strchr(temp, '\n');
                     if (newlineCheck)
                         *newlineCheck = '\0';
-                    namesModify(names,temp);
+                    strcpy(tempName.name,temp);
+                    tempName.count = 1;
+                    namesModify(names,tempName);
                 }
             }
             write(fd[1], names, NUM_NAME * sizeof(unique_name));
             fclose(fp);
+            exit(0);
         }
     }
-    if(child > 0) {
+        pid_t c;
         close(fd[1]);
         struct unique_name tempNames[NUM_NAME] = {0};
-        while (wait(NULL)) {
+        while ((c = wait(NULL)) != -1) {
             int index = 0;
             read(fd[0], tempNames, NUM_NAME * sizeof(unique_name));
-            while(strcmp(tempNames[index].name, "\0") == 0){
-                namesModify(names,tempNames[index].name);
+            printf("child %d display:\n", c);
+            display(tempNames);
+            while(tempNames[index].count != 0){
+                namesModify(names,tempNames[index]);
                 index++;
             }
-        }
+
     }
     display(names);
     return 0;
 }
-void namesModify(struct unique_name names[NUM_NAME], char tempName[NAME_LENGTH]){
+void namesModify(struct unique_name names[NUM_NAME], struct unique_name tempName){
     int index = 0;
     while (index < NUM_NAME) {
-        if (strcmp(names[index].name, "\0") == 0) {
-            strcpy(names[index].name, tempName);
-            names[index].count++;
+        if (names[index].count == 0) {
+            names[index] = tempName;
             index = NUM_NAME;
-        } else if (strcmp(names[index].name, tempName) == 0) {
-            names[index].count++;
+        } else if (strcmp(names[index].name, tempName.name) == 0) {
+            names[index].count += tempName.count;
             index = NUM_NAME;
         } else
             index++;
@@ -86,6 +90,8 @@ void namesModify(struct unique_name names[NUM_NAME], char tempName[NAME_LENGTH])
 // This function displays each unique name with its occurrence
 void display(struct unique_name names[NUM_NAME]) {
     int index = 0;
-    while(strcmp(names[index].name, "\0") == 0)
-        printf("%s: %d\n",names[index].name,names[index].count);
+    while(names[index].count != 0) {
+        printf("%s: %d\n", names[index].name, names[index].count);
+        index++;
+    }
 }
