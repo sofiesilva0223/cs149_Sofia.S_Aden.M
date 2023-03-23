14fc4df915246a5caadf2d0a42738f2f7b99b0f6
Sofia Silva <sofiasilva0223@gmail.com>
2023-03-23 16:20:25 -0700

    updated proc_manager.c
    Added comments and description to proc_manager.c

--- proc_manager.c
+++ proc_manager.c
@@ -1,9 +1,9 @@
 /**
- * Description:
+ * Description: Program reads command for each execution from stdin on one line of input and executes its parameters as specified on each line of stdin input. The proc_manager program will start up all the processes with the exec command and wait for them to complete, while each command will write its stdout and stderr to log files.
  * Author names: Aden Mengistie & Sofia Silva
  * Author emails: aden.mengistie@sjsu.edu, sofia.silva@sjsu.edu
- * Last modified date: 03/15/2023
- * Creation date: 03/22/2023
+ * Last modified date: 03/23/2023
+ * Creation date: 03/15/2023
  **/
 #include <stdio.h>
 #include <stdlib.h>
@@ -16,6 +16,7 @@
 #define COMMAND_LENGTH 30
 #define FILENAME_LENGTH 10
 
+//defining functions
 void parse_argument(char *command, char **argument);
 void open_files(pid_t child, char *filename_out, char *filename_err);
 
@@ -61,9 +62,10 @@ int main(int argc, char *argv[]) {
             open_files(c, filename_out, filename_err);
             printf("Finished child %d pid of parent %d\n",c,getpid());
             fflush(stdout);
-
+	    //exitcode
             if(WIFEXITED(status))
                 fprintf(stderr, "Exited with exitcode = %d\n", WEXITSTATUS(status));
+            //kill signal
             else if(WIFSIGNALED(status))
                 fprintf(stderr, "Killed with signal %d\n", WTERMSIG(status));
         }
@@ -77,6 +79,7 @@ int main(int argc, char *argv[]) {
     }
     return 0;
 }
+//function to parse argument
 void parse_argument(char *command, char **argument){
     int i = 0;
     char delimit[]= " \t\n";
@@ -87,6 +90,7 @@ void parse_argument(char *command, char **argument){
         argument[i] = strtok(NULL, delimit);
     }
 }
+//function for opening and writing stdout and stderr files for the child process
 void open_files(pid_t child, char *filename_out, char *filename_err){
     sprintf(filename_out,"%d.out",child);
     int file_desc1 = open(filename_out, O_RDWR | O_CREAT | O_APPEND, 0777);
