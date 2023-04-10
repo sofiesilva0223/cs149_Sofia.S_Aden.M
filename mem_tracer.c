ad2ddedb898d53c91faa103e8f31ae3509bbab68
adenmengistie <aden.mengistie@gmail.com>
2023-04-10 10:02:25 -0700

    final implementation including comments

--- mem_tracer.c
+++ mem_tracer.c
@@ -123,47 +123,53 @@ char* PRINT_TRACE(){
  * This function REALLOC calls realloc to expand the allocated memory
  * for command line input. It also prints the sequence of function calls
  * on the stack using PRINT_TRACE and the address segment assigned when
- * reallocating.
+ * reallocating memory.
  */
 void* REALLOC(void* p, int t, char* file, int line) {
     FILE *fp;
+    //open file
     fp = fopen(TRACE_FILENAME,"a");
-    if(fp == NULL){
+    if(fp == NULL){     //verify if unable to open file
         printf("Unable to open file: %s\n",TRACE_FILENAME);
         exit(1);
     }
-    p = realloc(p, t);
+    p = realloc(p, t);  //realloc
+    //program file (not the full file path)
     char* fn = strrchr(file,'/') ? strrchr(file,'/') : file;
+    //print to memtrace.out file - call PRINT_TRACE for sequence of function calls
     fprintf(fp,"File=\"%s\",Line=%d,Function=%s,Segment reallocation to address %p\n",fn,line,PRINT_TRACE(),p);
     fflush(fp);
-    fclose(fp);
+    fclose(fp);     //close file
     return p;
 }
 /**
  * This function MALLOC calls malloc to allocate memory for command
  * line input (initial memory allocation). It also prints the sequence of function calls
  * on the stack using PRINT_TRACE and the address segment assigned when
- * reallocating.
+ * allocating memory.
  */
 void* MALLOC(int t, char* file, int line) {
     FILE *fp;
+    //open file
     fp = fopen(TRACE_FILENAME,"a");
-    if(fp == NULL){
+    if(fp == NULL){     //verify if unable to open file
         printf("Unable to open file: %s\n",TRACE_FILENAME);
         exit(1);
     }
-    void* p = malloc(t);
+    void* p = malloc(t);    //allocate memory
+    //program file (not the full file path)
     char* fn = strrchr(file,'/') ? strrchr(file,'/') : file;
+    //print to memtrace.out file - call PRINT_TRACE for sequence of function calls
     fprintf(fp,"File=\"%s\",Line=%d,Function=%s,Segment allocation to address %p\n",fn,line,PRINT_TRACE(),p);
     fflush(fp);
-    fclose(fp);
+    fclose(fp);     //close file
     return p;
 }
 /**
  * This function FREE calls free to deallocate memory for the specified
  * command line input (void* p). It also prints the sequence of function calls
  * on the stack using PRINT_TRACE and the address segment assigned when
- * reallocating.
+ * freeing memory.
  */
 void FREE(void* p, char* file, int line) {
     FILE *fp;
@@ -174,94 +180,128 @@ void FREE(void* p, char* file, int line) {
         exit(1);
     }
     free(p);
+    //program file (not the full file path)
     char* fn = strrchr(file,'/') ? strrchr(file,'/') : file;
+    //print to memtrace.out file - call PRINT_TRACE for sequence of function calls
     fprintf(fp,"File=\"%s\",Line=%d,Function=%s,Segment at address %p deallocated\n",fn,line,PRINT_TRACE(),p);
     fflush(fp);
     fclose(fp);
 }
 
+//define memory realloc, malloc, free
 #define realloc(a,b) REALLOC(a,b,__FILE__,__LINE__)
 #define malloc(a) MALLOC(a,__FILE__,__LINE__)
 #define free(a) FREE(a,__FILE__,__LINE__)
 
+/**
+ * This function push the command line input and index passed through the
+ * char *p and int index. It stores the command lines and their index.
+ */
 void PUSH_COMMAND(char *p, int index){
     PUSH_TRACE("PUSH_COMMAND");
     COMMAND_NODE *currentNode;
 
+    //check if COMMAND_NODE_HEAD is NULL
     if(COMMAND_NODE_HEAD == NULL) {
+        //allocate COMMAND_NODE_HEAD memory
         COMMAND_NODE_HEAD = (COMMAND_NODE *) malloc(sizeof(COMMAND_NODE));
-        if (COMMAND_NODE_HEAD == NULL) {
+        if (COMMAND_NODE_HEAD == NULL) {    //verify if unable to allocate COMMAND_NODE_HEAD
             printf("PUSH_COMMAND: memory allocation error\n");
             exit(1);
         }
-        COMMAND_NODE_HEAD->command = p;
-        COMMAND_NODE_HEAD->index = index;
-        COMMAND_NODE_HEAD->next = NULL;
+        COMMAND_NODE_HEAD->command = p;     //assign ptr p to COMMAND_NODE_HEAD command
+        COMMAND_NODE_HEAD->index = index;   //assign index to COMMAND_NODE_HEAD index
+        COMMAND_NODE_HEAD->next = NULL;     //assign NULL to COMMAND_NODE_HEAD next
     }
-    else{
+    else{       //COMMAND_NODE_HEAD already allocated
+        //allocate currentNode memory
         currentNode = (COMMAND_NODE *) malloc(sizeof(COMMAND_NODE));
-        if(currentNode == NULL) {
+        if(currentNode == NULL) {   //verify if unable to allocate currentNode
             printf("PUSH_COMMAND: memory allocation error\n");
             exit(1);
         }
-        currentNode->command = p;
-        currentNode->index = index;
-        currentNode->next = COMMAND_NODE_HEAD;
-        COMMAND_NODE_HEAD = currentNode;
+        currentNode->command = p;       //assign ptr p to currentNode command
+        currentNode->index = index;     //assign index to currentNode index
+        currentNode->next = COMMAND_NODE_HEAD;  //assign ptr COMMAND_NODE_HEAD to currentNode next
+        COMMAND_NODE_HEAD = currentNode;        //assign ptr currentNode to COMMAND_NODE_HEAD
     }
     POP_TRACE();
 }
+/**
+ * This function prints the content (command and index) of the
+ * LinkedList recursively.
+ */
 void PRINT_COMMANDS(COMMAND_NODE *current){
     PUSH_TRACE("PRINT_COMMANDS");
+    //calls PRINT_COMMANDS until NULL
     if(current != NULL) {
         PRINT_COMMANDS(current->next);
         printf("Index: %d, Command: %s\n", current->index, current->command);
     }
     POP_TRACE();
 }
+/**
+ * This function free the LinkedList.
+ */
 void FREE_LIST(){
     PUSH_TRACE("FREE_LIST");
+    //loops until COMMAND_NODE_HEAD is NULL
     while(COMMAND_NODE_HEAD != NULL){
-        COMMAND_NODE *currentNode = COMMAND_NODE_HEAD;
-        COMMAND_NODE_HEAD = currentNode->next;
-        free(currentNode);
+        COMMAND_NODE *currentNode = COMMAND_NODE_HEAD;  //assign COMMAND_NODE_HEAD to currentNode
+        COMMAND_NODE_HEAD = currentNode->next;          //assign currentNode next to COMMAND_NODE_HEAD
+        free(currentNode);      //free currentNode
     }
     POP_TRACE();
 }
+/**
+ * This function frees the command line inputs and
+ * command line array.
+ */
 void free_commands(char **commands, int count){
     PUSH_TRACE("free_commands");
+    //loop through command array to free each command
     for(int i = 0; i <= count; i++)
         free(commands[i]);
-    free(commands);
+    free(commands);     //free command array
     POP_TRACE();
 }
 int main(int argc, char *argv[]) {
     PUSH_TRACE("main");
     //declare data
-    int allocatedElements = 0;
-    char **command;
+    int allocatedElements = 0;      //track for reallocation
+    char **command;                 //command array
 
+    //allocate command array
     command = (char **) malloc(ALLOCATION_ELEMENT * sizeof (char *));
-    allocatedElements = ALLOCATION_ELEMENT;
+    allocatedElements = ALLOCATION_ELEMENT;     //number of elements allocated thus far
     //traverse trough each file
-    int count = 0;
+    int count = 0;      //index tracking
+    //allocate for command input
     command[count] = (char *) malloc(BUFFER_LENGTH * sizeof (char));
+    //gets user input from stdin
     while (fgets(command[count], BUFFER_LENGTH, stdin)) {
+        //replace newline with null 
         char *temp = strchr(command[count],'\n');
         if(temp != NULL)
            temp = '\0';
 
-        PUSH_COMMAND(command[count],count + 1);
+        PUSH_COMMAND(command[count],count + 1);    //push command and index into LinkedList
         count++;
+        /**
+         *Verify if allocated memory for command array (number of elments)
+         *exceeds number of commands read, if so reallocate.
+         */
         if(count >= allocatedElements) {
-            PRINT_COMMANDS(COMMAND_NODE_HEAD);
-            allocatedElements = allocatedElements + ALLOCATION_ELEMENT;
+            PRINT_COMMANDS(COMMAND_NODE_HEAD);  //print content in LinkedList
+            allocatedElements = allocatedElements + ALLOCATION_ELEMENT;     //number of elements allocated thus far
             command = (char **) realloc(command, (allocatedElements) * sizeof(char *));
         }
+        //allocate for command input
         command[count] = (char *) malloc(BUFFER_LENGTH * sizeof (char));
     }
+    //call free_commands to free memory command input and command array
     free_commands(command, count);
-    FREE_LIST();
+    FREE_LIST();    //free LinkedList
     POP_TRACE();    //free main from stack
     POP_TRACE();    //free global from stack
     return 0;
